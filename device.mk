052ee9f9c565b814378aad25bf17ffe7e9a2b70b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-16 12:43:31 +0530

    x3:  Enable updating of APEXes
    Signed-off-by: bntxperses <dbikramyt@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit proprietary blobs
 $(call inherit-product, vendor/realme/x3/x3-vendor.mk)
 
